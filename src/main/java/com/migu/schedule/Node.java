1dd01fdc48bb6ae186c102da8bd3f50aebe38ed8
zhengshuangxi <zhengshuangxi@139.com>
2018-06-20 21:49:48 +0800

    提交人：郑双喜

---
+++ src/main/java/com/migu/schedule/Node.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.migu.schedule;
+
+import java.util.List;
+
+/**
+ * @author CHINA
+ *
+ */
+public class Node 
+{
+	private int nodeId;
+
+	//节点当前消耗
+	private int consumption;
+
+	private List<Task> taskList;
+
+	public int getNodeId() {
+		return nodeId;
+	}
+
+	public void setNodeId(int nodeId) {
+		this.nodeId = nodeId;
+	}
+
+	public List<Task> getTaskList() {
+		return taskList;
+	}
+
+	public void setTaskList(List<Task> taskList) {
+		this.taskList = taskList;
+	}
+
+	public int getConsumption() {
+		return consumption;
+	}
+
+	public void setConsumption(int consumption) {
+		this.consumption = consumption;
+	}
+}
